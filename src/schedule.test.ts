a00c97d79e8d77b7e3b2aced351c048a9ffa733d
Thibault “Adædra” Hamel <contact@adaedra.eu>
2019-01-12 16:15:24 +0100

    Improve tests
    * Use `process.nextTick` for better testing
    * Add a test for `undefined` case

--- src/schedule.test.ts
+++ src/schedule.test.ts
@@ -1,11 +1,25 @@
 import schedule from './schedule'
 
+const tick = async () => new Promise(done => process.nextTick(done))
+
 describe(schedule, () => {
     it('creates an observable and runs it once', async done => {
-        const obs = schedule('0 * * * * *', () => Promise.resolve(1))
-        obs.subscribe(v => {
-            expect(v).toEqual(1)
-            done()
-        })
+        const fn = jest.fn()
+        const obs = schedule('0 * * * * *', async () => 1)
+        obs.subscribe(fn)
+
+        await tick()
+        expect(fn).toHaveBeenCalledWith(1)
+        done()
+    })
+
+    it("doesn't call an observable if method resolves to undefined", async done => {
+        const fn = jest.fn()
+        const obs = schedule('0 * * * * *', async () => undefined)
+        obs.subscribe(fn)
+
+        await tick()
+        expect(fn).not.toHaveBeenCalled()
+        done()
     })
 })
