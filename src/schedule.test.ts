3feddec093fd319d9dccec4f3ba5b2c43dd2a6fe
Thibault “Adædra” Hamel <contact@adaedra.eu>
2019-01-12 16:25:31 +0100

    Test regular calls
    * Added test for scheduled calls execution

--- src/schedule.test.ts
+++ src/schedule.test.ts
@@ -1,8 +1,19 @@
+import lolex from 'lolex'
 import schedule from './schedule'
 
 const tick = async () => new Promise(done => process.nextTick(done))
 
 describe(schedule, () => {
+    let clock: lolex.Clock
+
+    beforeEach(() => {
+        clock = lolex.install()
+    })
+
+    afterEach(() => {
+        clock.uninstall()
+    })
+
     it('creates an observable and runs it once', async done => {
         const fn = jest.fn()
         const obs = schedule('0 * * * * *', async () => 1)
@@ -20,6 +31,21 @@ describe(schedule, () => {
 
         await tick()
         expect(fn).not.toHaveBeenCalled()
+        done()
+    })
+
+    it('calls regulary the generator', async done => {
+        const fn = jest.fn(async () => 0)
+        const obs = schedule('* * * * * *', fn)
+        obs.subscribe(() => {})
+
+        await tick()
+        expect(fn).toHaveBeenCalledTimes(1)
+
+        fn.mockClear()
+        clock.tick(3000)
+        expect(fn).toHaveBeenCalledTimes(3)
+
         done()
     })
 })
